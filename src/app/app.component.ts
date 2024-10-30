e18ac3e7156ae3368349414d5b1f36ed7b91da07
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-10-30 13:07:55 -0400

    initializes the dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
+
+  constructor(public dialog: MatDialog) { }
+  //This is the function that will open the dialog when the signup button is clicked
+opensUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+  // Assigning the dialog a width
+    width: '480px'
+    });
+  }
 }
