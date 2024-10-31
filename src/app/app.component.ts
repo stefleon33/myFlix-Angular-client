bf87b382c48e1a5e6970226f6bbd28b040b44f8f
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-10-31 13:55:13 -0400

    UserLoginFormComponent imported

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MatDialog } from '@angular/material/dialog';
 
 
@@ -15,6 +16,12 @@ export class AppComponent {
   //This is the function that will open the dialog when the signup button is clicked
 openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, {
+  // Assigning the dialog a width
+    width: '480px'
+    });
+  }
+openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
   // Assigning the dialog a width
     width: '480px'
     });
