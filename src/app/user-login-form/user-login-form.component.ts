1f2978adda659afbfe6a9172b9f41c9b267cfedf
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-10-31 13:56:11 -0400

    user login form initial commit

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+// You'll use this import to close the dialog on success
+import { MatDialogRef } from '@angular/material/dialog';
+
+// This import brings in the API calls we created in 6.2
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+// This import is used to display notifications back to the user
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-user-login-form',
   templateUrl: './user-login-form.component.html',
   styleUrl: './user-login-form.component.scss'
 })
-export class UserLoginFormComponent {
+export class UserLoginFormComponent implements OnInit {
 
+  @Input() userData = { Username: '', Password: '' };
+
+constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar) { }
+
+ngOnInit(): void {
 }
+
+// This is the function responsible for sending the form inputs to the backend
+loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe((result) => {
+  // Logic for a successful user login goes here! (To be implemented)
+     localStorage.setItem("user", JSON.stringify(result.user));
+     localStorage.setItem("token", result.token);
+     this.dialogRef.close(); // This will close the modal on success!
+     this.snackBar.open(`Login Successful, Hello ${result.user.Username}`, 'OK', {
+        duration: 2000
+     });
+    }, (result) => {
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
+    });
+  }
+
+  }
