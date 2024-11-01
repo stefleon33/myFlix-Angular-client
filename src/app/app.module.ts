6bb63667ef75074f7f57f3f41e7249413b371d3b
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-11-01 13:08:03 -0400

    router code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 //Angular Material imports
 import { MatInputModule } from '@angular/material/input';
@@ -16,10 +15,19 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+const appRoutes: Routes = [
+ {path: 'welcome', component: WelcomePageComponent },
+ {path: 'movies', component: MovieCardComponent },
+ {path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+];
 
 @NgModule({
   declarations: [
@@ -40,7 +48,9 @@ import { WelcomePageComponent } from './welcome-page/welcome-page.component';
     MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    RouterModule.forRoot(appRoutes),
+    MatIconModule
   ],
   providers: [
     provideAnimationsAsync()
--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -9,7 +9,8 @@ import { FetchApiDataService } from '../fetch-api-data.service';
 // This import is used to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
 
-
+import { Router } from '@angular/router';
+ 
 @Component({
   selector: 'app-user-login-form',
   templateUrl: './user-login-form.component.html',
@@ -22,7 +23,9 @@ export class UserLoginFormComponent implements OnInit {
 constructor(
     public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar) { }
+    public snackBar: MatSnackBar,
+    private router: Router
+ ) { }
 
 ngOnInit(): void {
 }
@@ -31,12 +34,13 @@ ngOnInit(): void {
 loginUser(): void {
     this.fetchApiData.userLogin(this.userData).subscribe((result) => {
   // Logic for a successful user login goes here! (To be implemented)
-     localStorage.setItem("user", JSON.stringify(result.user));
+     localStorage.setItem("user", JSON.stringify(result.user.Username));
      localStorage.setItem("token", result.token);
      this.dialogRef.close(); // This will close the modal on success!
      this.snackBar.open(`Login Successful, Hello ${result.user.Username}`, 'OK', {
         duration: 2000
      });
+     this.router.navigate(['movies']);
     }, (result) => {
       this.snackBar.open(result, 'OK', {
         duration: 2000
