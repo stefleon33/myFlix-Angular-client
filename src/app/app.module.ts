2099419c13511d7049a8864d857921068d979c92
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-11-04 11:45:47 -0500

    path added for user profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 //Angular Material imports
 import { MatInputModule } from '@angular/material/input';
@@ -21,12 +22,12 @@ import { UserRegistrationFormComponent } from './user-registration-form/user-reg
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 
 const appRoutes: Routes = [
  {path: 'welcome', component: WelcomePageComponent },
  {path: 'movies', component: MovieCardComponent },
+ {path: 'profile', component: UserProfileComponent },
  {path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
 ];
 
