2e0b9e04a000640db05370d132f0cccd55941e7e
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-11-04 17:48:10 -0500

    formatting

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,9 +1,9 @@
 // src/app/movie-card/movie-card.component.ts
 import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service'
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { MessageBoxComponent } from '../message-box/message-box.component';
 
 @Component({
@@ -15,87 +15,99 @@ export class MovieCardComponent implements OnInit {
   movies: any[] = [];
   constructor(
     public fetchApiData: FetchApiDataService,
-    public dialog:MatDialog,
-    public router: Router
+    public dialog: MatDialog,
+    public router: Router,
+    public snackbar: MatSnackBar
   ) { }
 
-ngOnInit(): void {
-  this.getMovies();
-}
+  ngOnInit(): void {
+    this.getMovies();
+  }
 
-//Function to get all movies using FetchApiDataService
-getMovies(): void {
-  this.fetchApiData
-    .getAllMovies()
-    .subscribe((resp: any) => {
-      this.movies = resp;
+  //Function to get all movies using FetchApiDataService
+  getMovies(): void {
+    this.fetchApiData
+      .getAllMovies()
+      .subscribe((resp: any) => {
+        this.movies = resp;
 
-      let user = JSON.parse(localStorage.getItem("user") || "");
-      this.movies.forEach((movie: any) => {
-        movie.isFavorite = user.favoriteMovies.includes(movie._id);
+        let user = JSON.parse(localStorage.getItem("user") || "");
+        this.movies.forEach((movie: any) => {
+          movie.isFavorite = user.favoriteMovies.includes(movie._id);
+        })
+        return this.movies;
+    }, err => {
+      console.error(err)
+    })
+  }
+
+  showGenre(movie: any): void {
+    this.dialog.open(MessageBoxComponent, {
+      data: {
+        title: String(movie.Genre.Name),
+        content: movie.Genre.Description
+      },
+      width: "400px"
+    })
+  }
+
+  showDirector(movie: any): void {
+    this.dialog.open(MessageBoxComponent, {
+      data: {
+        title: String(movie.Director.Name),
+        content: movie.Director.Bio
+      },
+      width: "400px"
+    })
+  }
+
+  showDescription(movie: any): void {
+    this.dialog.open(MessageBoxComponent, {
+      data: {
+        content: movie.Description
+      },
+      width: "400px"
+    })
+  }
+
+  modifyFavoriteMovies(movie: any): void {
+    let user = JSON.parse(localStorage.getItem("user") || "" ); 
+    let icon = document.getElementById(`${movie._id}-favorite-icon`);
+
+    if (user.favoriteMovies.includes(movie._id)) {
+      this.fetchApiData.deleteUserFavoriteMovies(user.id, movie.title).subscribe(res => {
+        icon?.setAttribute("fontIcon", "favorite_border");
+        
+        user.favoriteMovies = res.favoriteMovies;
+        localStorage.setItem("user", JSON.stringify(user));
+        this.snackbar.open(`Movie removed from favorties`, 'OK', {
+          duration: 2000
+        });
+      }, err => {
+        console.error(err)
       })
-      return this.movies;
-  }, err => {
-    console.error(err)
-  })
-}
+    } else {
+      this.fetchApiData.addUserFavoriteMovies(user.id, movie.title).subscribe(res => {
+        icon?.setAttribute("fontIcon", "favorite_border");
+        
+        user.favoriteMovies = res.favoriteMovies;
+        localStorage.setItem("user", JSON.stringify(user));
+        this.snackbar.open(`Movie added to favorties`, 'OK', {
+          duration: 2000
+        });
+      }, err => {
+        console.error(err)
+    })
+  }
+    localStorage.setItem("user", JSON.stringify(user));
+  }
 
-showGenre(movie: any): void {
-  this.dialog.open(MessageBoxComponent, {
-    data: {
-      title: String(movie.Genre.Name),
-      content: movie.Genre.Description
-    },
-    width: "400px"
-  })
-}
+  redirectProfile(): void {
+    this.router.navigate(["profile"]);
+  }
 
-showDirector(movie: any): void {
-  this.dialog.open(MessageBoxComponent, {
-    data: {
-      title: String(movie.Director.Name),
-      content: movie.Director.Bio
-    },
-    width: "400px"
-  })
-}
-
-showDescription(movie: any): void {
-  this.dialog.open(MessageBoxComponent, {
-    data: {
-      content: movie.Description
-    },
-    width: "400px"
-  })
-}
-
-/*
-//Function to add to favorites using FetchApiDataService
-addToFavorties(movieTitle: string): void {
-  const user: any = JSON.parse(localStorage.getItem('user') as any);
-    this.fetchApiData
-      .addUserFavoriteMovies(user.Username, movieTitle)
-      .subscribe((res:any) => {
-        console.log(res);
-        this.getMovies();
-      });
-      this.snackBar.open("Movie added to favorites", 'OK', {
-        duration: 2000
-      });
-  };
-
-//Function to remove favorites using FetchApiDataService
-deleteFromFavorties(movieTitle: string): void {
-  const user: any = JSON.parse(localStorage.getItem('user') as any);
-    this.fetchApiData
-      .deleteUserFavoriteMovies(user.Username, movieTitle)
-      .subscribe((res:any) => {
-        console.log(res);
-        this.getMovies();
-      });
-      this.snackBar.open("Movie removed from favorites", 'OK', {
-        duration: 2000
-      });
-  };
-*/
+  logout(): void {
+    this.router.navigate(["welcome"]);
+    localStorage.removeItem("user");
+  }
 }
