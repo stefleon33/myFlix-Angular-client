9c3f8f7700d1c13cacb52f58b611f90ea978d33f
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-11-04 16:15:28 -0500

    snackbar changed to dialog

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -2,18 +2,20 @@
 import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service'
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Router } from 'express';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { MessageBoxComponent } from '../message-box/message-box.component';
 
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
   styleUrls: ['./movie-card.component.scss']
 })
-export class MovieCardComponent {
+export class MovieCardComponent implements OnInit {
   movies: any[] = [];
   constructor(
     public fetchApiData: FetchApiDataService,
-    public snackBar:MatSnackBar,
+    public dialog:MatDialog,
     public router: Router
   ) { }
 
@@ -27,23 +29,47 @@ getMovies(): void {
     .getAllMovies()
     .subscribe((resp: any) => {
       this.movies = resp;
-      console.log(this.movies);
+
+      let user = JSON.parse(localStorage.getItem("user") || "");
+      this.movies.forEach((movie: any) => {
+        movie.isFavorite = user.favoriteMovies.includes(movie._id);
+      })
       return this.movies;
-  });
+  }, err => {
+    console.error(err)
+  })
 }
 
-showGenreAlert(genre: any): void {
-  alert(genre);
+showGenre(movie: any): void {
+  this.dialog.open(MessageBoxComponent, {
+    data: {
+      title: String(movie.Genre.Name),
+      content: movie.Genre.Description
+    },
+    width: "400px"
+  })
 }
 
-showDirectorAlert(director: any): void {
-  alert(director);
+showDirector(movie: any): void {
+  this.dialog.open(MessageBoxComponent, {
+    data: {
+      title: String(movie.Director.Name),
+      content: movie.Director.Bio
+    },
+    width: "400px"
+  })
 }
 
-showDescriptionAlert(description: any): void {
-  alert(description);
+showDescription(movie: any): void {
+  this.dialog.open(MessageBoxComponent, {
+    data: {
+      content: movie.Description
+    },
+    width: "400px"
+  })
 }
 
+/*
 //Function to add to favorites using FetchApiDataService
 addToFavorties(movieTitle: string): void {
   const user: any = JSON.parse(localStorage.getItem('user') as any);
@@ -53,7 +79,7 @@ addToFavorties(movieTitle: string): void {
         console.log(res);
         this.getMovies();
       });
-      this.snackBar.open("movie added to favorites", 'OK', {
+      this.snackBar.open("Movie added to favorites", 'OK', {
         duration: 2000
       });
   };
@@ -67,9 +93,9 @@ deleteFromFavorties(movieTitle: string): void {
         console.log(res);
         this.getMovies();
       });
-      this.snackBar.open("movie removed from favorites", 'OK', {
+      this.snackBar.open("Movie removed from favorites", 'OK', {
         duration: 2000
       });
   };
-
+*/
 }
