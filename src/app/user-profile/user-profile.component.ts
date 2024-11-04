c271439545336197b3a90bbaeebb598e1ccccd4d
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-11-04 17:50:13 -0500

    imports for user profile

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service'
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from  '@angular/router';
 
 @Component({
   selector: 'app-user-profile',
