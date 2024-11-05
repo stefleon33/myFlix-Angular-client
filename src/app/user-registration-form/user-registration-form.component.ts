23e3e67f4ee557d9b16bef86c0bc3388282243b4
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-11-05 09:52:30 -0500

    formatting

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -31,9 +31,8 @@ ngOnInit(): void {
 // This is the function responsible for sending the form inputs to the backend
 registerUser(): void {
     this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
-  // Logic for a successful user registration goes here! (To be implemented)
-     this.dialogRef.close(); // This will close the modal on success!
-     this.snackBar.open(result, 'OK', {
+      this.dialogRef.close(); // This will close the modal on success!
+      this.snackBar.open(result, 'OK', {
         duration: 2000
      });
     }, (result) => {
